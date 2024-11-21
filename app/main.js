276c74c591074899c32906b2ca39e2570b52c0bb
Rafa MZ <mz.tears.of.time@gmail.com>
2024-11-21 16:16:40 -0300

    5. Exibindo livros no site

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 let livros = [];
 const end_Point_Da_API = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+const elemento_Para_Inserir_Livros = document.getElementById ( 'livros' );
+
 
 get_Buscar_Livros_Da_API ();
 
@@ -8,4 +10,28 @@ async function get_Buscar_Livros_Da_API ()
 	const resposta = await fetch ( end_Point_Da_API );
 	livros = await resposta.json();
 	console.table ( livros );
+
+	Exibir_Livros_Na_Tela ( livros );
+}
+
+function Exibir_Livros_Na_Tela ( lista_de_livros )
+{
+	lista_de_livros.forEach ( livro =>
+	{
+		elemento_Para_Inserir_Livros.innerHTML +=
+		`
+			<div class="livro">
+				<img class="livro__imagens" src="${livro.imagem}"
+					alt = "${livro.alt}" />
+				<h2 class="livro__titulo">
+					${livro.titulo}
+				</h2>
+				<p class="livro__descricao">${livro.autor}</p>
+				<p class="livro__preco" id="preco">R$${livro.preco}</p>
+				<div class="tags">
+					<span class="tag">${livro.categoria}</span>
+				</div>
+			</div>
+		`
+	})
 }
