014338a98c2b7623c2f750366961d283b4575d34
Rafa MZ <mz.tears.of.time@gmail.com>
2024-12-10 16:55:14 -0300

    9. Recebendo elemento de forma dinâmica

--- app/metodo_Filter.js
+++ app/metodo_Filter.js
@@ -1,9 +1,16 @@
-const btn_Filtrar_Livros_De_Front = document.getElementById ( "btnFiltrarLivrosFront" );
+// const btn_Filtrar_Livros_De_Front = document.getElementById ( "btnFiltrarLivrosFront" );
 
-btn_Filtrar_Livros_De_Front.addEventListener ( 'click', Filtrar_Livros );
+// btn_Filtrar_Livros_De_Front.addEventListener ( 'click', Filtrar_Livros );
+
+const botoes = document.querySelectorAll ( ".btn" );
+
+botoes.forEach ( btn => btn.addEventListener ( 'click', Filtrar_Livros ))
 
 function Filtrar_Livros ()
 {
-	let livros_Filtrados = livros.filter ( livro => livro.categoria == 'front-end' );
+	const elemento_Btn = document.getElementById ( this.id );
+	const categoria = elemento_Btn.value;
+
+	let livros_Filtrados = livros.filter ( livro => livro.categoria == categoria );
 	console.table ( livros_Filtrados );
 }
